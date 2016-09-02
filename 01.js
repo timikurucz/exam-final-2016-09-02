bbf8edc8e32bef2176fb5fc21082356ef4e04288
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-09-02 09:07:58 +0200

    assignment

---
+++ 01.js
@@ -0,0 +1,27 @@
+// Create a function that counts the cars by colors
+// With an output like: {red: 3, yellow: 2, white: 1, green: 1}
+
+
+const cars = [{
+    color: 'red',
+    type: 'Volvo'
+  }, {
+    color: 'yellow',
+    type: 'VW'
+  }, {
+    color: 'red',
+    type: 'Ferrari'
+  }, {
+    color: 'white',
+    type: 'Tesla'
+  }, {
+    color: 'yellow',
+    type: 'Toyota'
+  }, {
+    color: 'red',
+    type: 'Lada'
+  }, {
+    color: 'green',
+    type: 'Trabant'
+}];
+
